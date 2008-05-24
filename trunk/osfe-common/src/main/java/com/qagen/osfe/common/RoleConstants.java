af332e0133e9dee6fa82e9d93e5419ed45534bb3
gdboling@gmail.com <gdboling@gmail.com@d91a282b-a64b-0410-bc40-a1d30b429491>
2008-05-24 02:20:36 +0000

    initial import

---
+++ trunk/osfe-common/src/main/java/com/qagen/osfe/common/RoleConstants.java
@@ -0,0 +1,27 @@
+/* Copyright 2008 Hycel Taylor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.qagen.osfe.common;
+
+/**
+ * Author: Gregg Bolinger
+ * <p/>
+ */
+public class RoleConstants {
+
+  public static final String ADMINISTRATOR = "Administrator";
+  public static final String FEED_MANAGER = "Feed Manager";
+  public static final String DATA_MANAGER = "Data Manager";
+  public static final String USER = "User";
+}
