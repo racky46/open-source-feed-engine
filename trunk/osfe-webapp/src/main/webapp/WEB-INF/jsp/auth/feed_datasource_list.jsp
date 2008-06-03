b4a8c33fe2d0dde1128c1b0f3df83e96a43064c6
gdboling@gmail.com <gdboling@gmail.com@d91a282b-a64b-0410-bc40-a1d30b429491>
2008-06-03 18:21:01 +0000

    added support for search

--- trunk/osfe-webapp/src/main/webapp/WEB-INF/jsp/auth/feed_datasource_list.jsp
+++ trunk/osfe-webapp/src/main/webapp/WEB-INF/jsp/auth/feed_datasource_list.jsp
@@ -3,11 +3,14 @@
 <s:layout-render name="/layout/default.jsp">
   <s:layout-component name="html-head">
     <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/styles/jqGrid/themes/basic/grid.css"/>
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/styles/jqGrid/themes/jqModal.css"/>
+    <script type="text/javascript" src="${pageContext.request.contextPath}/scripts/jquery/jqGrid/js/jqModal.js"></script>
+    <script type="text/javascript" src="${pageContext.request.contextPath}/scripts/jquery/jqGrid/js/jqDnR.js"></script>
     <script type="text/javascript" src="${pageContext.request.contextPath}/scripts/jquery/jqGrid/jquery.jqGrid.js"></script>
     <script type="text/javascript" src="${pageContext.request.contextPath}/scripts/feed_datasource_list.js"></script>
   </s:layout-component>
   <s:layout-component name="contents">
     <table id="feedDatasourceList" class="scroll" cellpadding="0" cellspacing="0" ></table>
-    <div id="pager" class="scroll"></div>
+    <div id="pager" class="scroll" style="text-align:center;"></div>
   </s:layout-component>
 </s:layout-render>
--- trunk/osfe-webapp/src/main/webapp/scripts/feed_datasource_list.js
+++ trunk/osfe-webapp/src/main/webapp/scripts/feed_datasource_list.js
@@ -2,10 +2,11 @@
         $('#feedDatasourceList').jqGrid({
             url: '/app/action/feed/datasources/list',
             datatype: 'json',
-            colNames:['Name','Description'],
+            colNames:['Name','Description','Actions'],
             colModel:[
                 {name:'feedDataSourceId',index:'feedDataSourceId',width:100},
-                {name:'description',index:'description',width:100}
+                {name:'description',index:'description',width:100},
+                {index:'actions',width:100,sortable:false}
             ],
             caption: 'Feed Data Source List',
             rowNum: 25,
@@ -15,6 +16,14 @@
             sortname: 'name',
             sortorder: 'asc',
             width: 400,
-            height: 200
-        });
+            height: 200,
+            loadComplete: function() {
+
+              $('a[title=delete]').click(function() {
+                alert("Going to delete " + this.id);
+              });
+
+              $('#feedDatasourceList a').css("cursor", "pointer");
+            }
+        }).navGrid('#pager', {edit:false, add:false, del:false},{},{},{},{sopt:['bw','eq','ew','cn']});
     });
