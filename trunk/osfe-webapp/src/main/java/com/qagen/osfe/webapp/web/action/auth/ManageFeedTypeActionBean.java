69f7327edeb621dfd076f37c006d2bd1069f264c
gdboling@gmail.com <gdboling@gmail.com@d91a282b-a64b-0410-bc40-a1d30b429491>
2008-06-26 19:01:42 +0000

    modified and added code to get generic menu system and home page

--- trunk/osfe-webapp/src/main/java/com/qagen/osfe/webapp/web/action/auth/ManageFeedTypeActionBean.java
+++ trunk/osfe-webapp/src/main/java/com/qagen/osfe/webapp/web/action/auth/ManageFeedTypeActionBean.java
@@ -39,143 +39,144 @@ import java.util.List;
 @UrlBinding("/action/feed/type/{$event}/{feedType.feedTypeId}/{page}/{rows}/{sidx}/{sord}")
 public class ManageFeedTypeActionBean extends BaseActionBean {
 
-    @ValidateNestedProperties({
-    @Validate(field = "feedTypeId", required = true),
-    @Validate(field = "description", required = true)
-            })
-    private FeedType feedType;
-    private FeedTypeService feedTypeService;
+  @ValidateNestedProperties({
+  @Validate(field = "feedTypeId", required = true),
+  @Validate(field = "description", required = true)
+      })
+  private FeedType feedType;
+  private FeedTypeService feedTypeService;
 
-    private Integer page = 1;
-    private Integer rows = 10;
-    private String sidx = "feedTypeId";
-    private String sord = "asc";
+  private Integer page = 1;
+  private Integer rows = 10;
+  private String sidx = "feedTypeId";
+  private String sord = "asc";
 
-    public FeedType getFeedType() {
-        return feedType;
+  public FeedType getFeedType() {
+    return feedType;
+  }
+
+  public void setFeedType(FeedType feedType) {
+    this.feedType = feedType;
+  }
+
+  @SpringBean(FeedTypeService.SERVICE_ID)
+  public void setDataSourceService(FeedTypeService feedTypeService) {
+    this.feedTypeService = feedTypeService;
+  }
+
+  public Integer getPage() {
+    return page;
+  }
+
+  public void setPage(Integer page) {
+    this.page = page;
+  }
+
+  public Integer getRows() {
+    return rows;
+  }
+
+  public void setRows(Integer rows) {
+    this.rows = rows;
+  }
+
+  public String getSidx() {
+    return sidx;
+  }
+
+  public void setSidx(String sidx) {
+    this.sidx = sidx;
+  }
+
+  public String getSord() {
+    return sord;
+  }
+
+  public void setSord(String sortd) {
+    this.sord = sortd;
+  }
+
+  @DefaultHandler
+  @DontBind
+  public Resolution display() {
+    return new ForwardResolution(FEED_TYPE_LIST_VIEW);
+  }
+
+  @DontBind
+  public Resolution add() {
+    return new ForwardResolution(FEED_TYPE_MODIFY_VIEW);
+  }
+
+  @DontValidate
+  public Resolution edit() {
+    if (feedType.getFeedTypeId() != null) {
+      setEditMode(true);
+      feedType = feedTypeService.findByPrimaryId(feedType.getFeedTypeId());
+    }
+    return new ForwardResolution(FEED_TYPE_MODIFY_VIEW);
+  }
+
+  public Resolution save() {
+    System.out.println(isEditMode());
+    if (!isEditMode()) {
+      feedTypeService.insert(feedType);
+    } else {
+      System.out.println("Updating");
+      feedTypeService.update(feedType);
+    }
+    getContext().getMessages().add(new LocalizableMessage("feedTypeSaveSuccess"));
+    return new RedirectResolution(ManageFeedTypeActionBean.class);
+  }
+
+  @DontValidate
+  public Resolution delete() {
+    System.out.println(feedType.getFeedTypeId());
+    feedTypeService.delete(feedType);
+    return null;
+  }
+
+  @DontValidate
+  public Resolution cancel() {
+    return new RedirectResolution(ManageFeedTypeActionBean.class);
+  }
+
+  @DontValidate
+  public Resolution list() {
+
+    List<FeedType> feeds = feedTypeService.findAll();
+
+    double val = 0;
+    double totalPages = 0;
+    if (feeds.size() >= new Double(rows)) {
+      val = feeds.size() / new Double(rows);
+      totalPages = Math.ceil(val);
+    } else {
+      totalPages = 1;
     }
 
-    public void setFeedType(FeedType feedType) {
-        this.feedType = feedType;
+    int start = rows * page - rows;
+    feeds = feedTypeService.findAllWithLimit(new LimitParam(start, rows));
+
+    JqGridJsonModel json = new JqGridJsonModel();
+    json.setPage(String.valueOf(page));
+    json.setRecords(String.valueOf(rows));
+    json.setTotal(((int) totalPages));
+
+    List<JqGridRow> rows = new ArrayList<JqGridRow>();
+    for (FeedType ft : feeds) {
+      JqGridRow row = new JqGridRow();
+      row.setId(ft.getFeedTypeId());
+      List<String> cells = new ArrayList<String>();
+      cells.add(ft.getFeedTypeId());
+      cells.add(ft.getDescription());
+      cells.add("action");
+      row.setCell(cells);
+      rows.add(row);
     }
+    json.setRows(rows);
 
-    @SpringBean(FeedTypeService.SERVICE_ID)
-    public void setDataSourceService(FeedTypeService feedTypeService) {
-        this.feedTypeService = feedTypeService;
-    }
-
-    public Integer getPage() {
-        return page;
-    }
-
-    public void setPage(Integer page) {
-        this.page = page;
-    }
-
-    public Integer getRows() {
-        return rows;
-    }
-
-    public void setRows(Integer rows) {
-        this.rows = rows;
-    }
-
-    public String getSidx() {
-        return sidx;
-    }
-
-    public void setSidx(String sidx) {
-        this.sidx = sidx;
-    }
-
-    public String getSord() {
-        return sord;
-    }
-
-    public void setSord(String sortd) {
-        this.sord = sortd;
-    }
-
-    @DefaultHandler
-    @DontBind
-    public Resolution display() {
-        return new ForwardResolution(FEED_TYPE_LIST_VIEW);
-    }
-
-    @DontBind
-    public Resolution add() {
-        return new ForwardResolution(FEED_TYPE_MODIFY_VIEW);
-    }
-
-    @DontValidate
-    public Resolution edit() {
-        if (feedType.getFeedTypeId() != null) {
-            setEditMode(true);
-            feedType = feedTypeService.findByPrimaryId(feedType.getFeedTypeId());
-        }
-        return new ForwardResolution(FEED_TYPE_MODIFY_VIEW);
-    }
-
-    public Resolution save() {
-        System.out.println(isEditMode());
-        if (!isEditMode()) {
-            feedTypeService.insert(feedType);
-        } else {
-            System.out.println("Updating");
-            feedTypeService.update(feedType);
-        }
-        getContext().getMessages().add(new LocalizableMessage("feedTypeSaveSuccess"));
-        return new RedirectResolution(ManageFeedTypeActionBean.class);
-    }
-
-    @DontValidate
-    public Resolution delete() {
-        feedTypeService.delete(feedType);
-        return null;
-    }
-
-    @DontValidate
-    public Resolution cancel() {
-        return new RedirectResolution(ManageFeedTypeActionBean.class);
-    }
-
-    @DontValidate
-    public Resolution list() {
-
-        List<FeedType> feeds = feedTypeService.findAll();
-
-        double val = 0;
-        double totalPages = 0;
-        if (feeds.size() >= new Double(rows)) {
-            val = feeds.size() / new Double(rows);
-            totalPages = Math.ceil(val);
-        } else {
-            totalPages = 1;
-        }
-
-        int start = rows * page - rows;
-        feeds = feedTypeService.findAllWithLimit(new LimitParam(start, rows));
-
-        JqGridJsonModel json = new JqGridJsonModel();
-        json.setPage(String.valueOf(page));
-        json.setRecords(String.valueOf(rows));
-        json.setTotal(((int) totalPages));
-
-        List<JqGridRow> rows = new ArrayList<JqGridRow>();
-        for (FeedType ft : feeds) {
-            JqGridRow row = new JqGridRow();
-            row.setId(ft.getFeedTypeId());
-            List<String> cells = new ArrayList<String>();
-            cells.add(ft.getFeedTypeId());
-            cells.add(ft.getDescription());
-            cells.add("action");
-            row.setCell(cells);
-            rows.add(row);
-        }
-        json.setRows(rows);
-
-        JSONSerializer serializer = new JSONSerializer();
-        String jsonResult = serializer.exclude("*.class").deepSerialize(json);
-        return new StreamingResolution("text/javascript", new StringReader(jsonResult));
-    }
+    JSONSerializer serializer = new JSONSerializer();
+    String jsonResult = serializer.exclude("*.class").deepSerialize(json);
+    return new StreamingResolution("text/javascript", new StringReader(jsonResult));
+  }
 }
--- trunk/osfe-webapp/src/main/webapp/layout/_header.jsp
+++ trunk/osfe-webapp/src/main/webapp/layout/_header.jsp
@@ -1,2 +1,10 @@
-<h1>Header</h1>
+<%@ taglib prefix="s" uri="http://stripes.sourceforge.net/stripes.tld" %>
+<div id="header">
+  <h2>OSFE Administration Console</h2>
+  <s:link beanclass="com.qagen.osfe.webapp.web.action.auth.HomeActionBean">Home</s:link> |
+  <s:link beanclass="com.qagen.osfe.webapp.web.action.auth.ManageFeedDatasourcesActionBean">Manage Data Sources</s:link> |
+  <s:link beanclass="com.qagen.osfe.webapp.web.action.auth.ManageFeedGroupActionBean">Manage Groups</s:link> |
+  <s:link beanclass="com.qagen.osfe.webapp.web.action.auth.ManageFeedTypeActionBean">Manage Types</s:link> |
+  <s:link beanclass="com.qagen.osfe.webapp.web.action.auth.ManageFeedsActionBean">Manage Feeds</s:link>
+</div>
 <hr/>
