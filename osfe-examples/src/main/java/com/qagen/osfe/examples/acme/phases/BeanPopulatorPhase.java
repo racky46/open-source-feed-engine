2addf57a315b5e8bd6dbfddb587db4a35fc257a6
hycel1 <hycel1@gmail.com>
2008-07-01 12:25:34 +0000

    BeanPopulatorPhase has been abstracted up into RowBeanPopulatorPhase.  Bean populators should extend RowBeanPopulator

--- osfe-examples/src/main/java/com/qagen/osfe/examples/acme/phases/BeanPopulatorPhase.java
+++ osfe-examples/src/main/java/com/qagen/osfe/examples/acme/phases/BeanPopulatorPhase.java
@@ -18,49 +18,39 @@ package com.qagen.osfe.examples.acme.phases;
 import com.qagen.osfe.core.EngineContext;
 import com.qagen.osfe.core.Phase;
 import com.qagen.osfe.core.Splitter;
+import com.qagen.osfe.core.phases.RowBeanPopulatorPhase;
 import com.qagen.osfe.core.row.RowValue;
+import com.qagen.osfe.core.row.Row;
 import com.qagen.osfe.core.utils.BeanPopulator;
 import com.qagen.osfe.examples.acme.row.DetailRow;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class BeanPopulatorPhase extends Phase {
-  private Splitter splitter;
-  private Integer batchSize;
-  private List<DetailRow> rows;
+/**
+ * Author: Hycel Taylor
+ * <p/>
+ * This BeanPopulatorPhase specifically populates the DetailRow object.
+ */
+public class BeanPopulatorPhase extends RowBeanPopulatorPhase {
 
+  /**
+   * Constructor
+   *
+   * @param context reference to the engine context.
+   * @param name    uniquely identifies the given phase.
+   */
   public BeanPopulatorPhase(EngineContext context, String name) {
     super(context, name);
   }
 
-  public void initialize() {
-    splitter = context.getDetailSplitter();
-    batchSize = context.getBatchSize();
-    rows = new ArrayList<DetailRow>();
-  }
-
-  public void execute() {
-    rows.clear();
-
-    Integer batchCounter = 0;
-    while (splitter.hasNextRow() && (batchCounter++ < batchSize)) {
-      final List<RowValue> rowValues = splitter.getNextRow();
-      final DetailRow detailRow = new DetailRow();
-
-      BeanPopulator.populateBean(rowValues, detailRow);
-      rows.add(detailRow);
-    }
-
-    context.setRows(rows);
-  }
-
   /**
-   * The method should be used to close resources. Any core FifeCycleService
-   * should make sure to call the shutdown() method in all phases as a final
-   * processe of its life cycle.
+   * Instantiate the DetaiRow object that will loaded with values from the
+   * data within a given row of a given feed file.
+   *
+   * @return reference to a DetailRow object.
    */
-  public void shutdown() {
-    // No resources to close...
+  public Row getNewRow() {
+    return new DetailRow();
   }
 }
